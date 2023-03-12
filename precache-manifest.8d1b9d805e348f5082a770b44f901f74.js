92807746d84b750ed5c182f09f9135e81ddae5a5
Saar Barak <59158336+saarbk@users.noreply.github.com>
2023-03-12 20:31:45 +0000

    Updates

---
+++ precache-manifest.8d1b9d805e348f5082a770b44f901f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d91c65960839a6e40bf",
+    "url": "/static/css/main.bac56b49.chunk.css"
+  },
+  {
+    "revision": "8d91c65960839a6e40bf",
+    "url": "/static/js/main.8d91c659.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "ac9acdb488411e9dc5c2",
+    "url": "/static/css/2.d1b9a66e.chunk.css"
+  },
+  {
+    "revision": "ac9acdb488411e9dc5c2",
+    "url": "/static/js/2.ac9acdb4.chunk.js"
+  },
+  {
+    "revision": "df568333923053498481882363861aa4",
+    "url": "/index.html"
+  }
+];
